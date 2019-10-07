20f07f0c8e2043bea92061547e9300b02082f265
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 11:22:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,13 @@
 def nyc_pigeon_organizer(data)
   hash = {}
-  pigeon_list = {}
   data.each do |trait, attributes|
     attributes.each do |details, names|
       names.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][trait] ||= [] 
-        pigeon_list[name][trait] << details.to_s
+        hash[name] ||= {}
+        hash[name][trait] ||= [] 
+        hash[name][trait] << details.to_s
       end
     end
   end
-  return pigeon_list
+  return hash
 end
